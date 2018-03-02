c3561b76034b9b3da9ba3070ea7d0be6837d1e04
Etiendo <32451923+Etiendo@users.noreply.github.com>
2018-03-02 09:48:43 +0100

    V1 PHP natif Login

---
+++ projet_cd/model/add_poi.php
@@ -0,0 +1,36 @@
+<?php
+
+function addPoi($name, $lat, $lng, $map_id)
+{
+    require 'config.php';
+
+    try {
+        $pdo = new PDO($dsn, $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo 'Connexion échouée : ' . $e->getMessage();
+    }
+
+    try {
+        $sql = ("INSERT INTO pois (name, lat, lng, map_id) VALUES (:name, :lat, :lng, :map_id)");
+
+        $statement = $sql->prepare();
+
+        $statement->bindParam(':name', $name);
+        $statement->bindParam(':lat', $lat);
+        $statement->bindParam(':lng', $lng);
+        $statement->bindParam(':map_id', $map_id);
+
+        if ($statement->execute()) {
+            echo "POI ajouté !";
+        } else {
+            echo "Ajout non effectué";
+        }
+
+        return $statement;
+    } catch (PDOException $e) {
+        echo 'Connexion échouée : ' . $e->getMessage();
+    }
+}
+
+header('location: map_browse.php');
